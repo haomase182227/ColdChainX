83be3b57deb20e3219c5be601a7301499d587c71
ThangDVSE183732 <thangdvse183732@fpt.edu.vn>
2026-06-06 22:38:37 +0700

    fix: resolve merge conflicts in main

---
+++ ColdChainX.Core/Enums/Role.cs
@@ -0,0 +1,11 @@
+namespace ColdChainX.Core.Enums
+{
+    public enum Role
+    {
+        Admin = 1,
+        Dispatcher = 2,
+        Sales = 3,
+        Customer = 4,
+        Driver = 5
+    }
+}
