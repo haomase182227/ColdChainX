968fa3da375fc0cf3b0a6f42b1001f50bf30dafc
bibongvy147 <tranlesiquynh3421@gmail.com>
2026-06-19 00:21:39 +0700

    [FILES] Align attachment upload size validation

--- ColdChainX.Application/Validators/UploadAttachmentRequestValidator.cs
+++ ColdChainX.Application/Validators/UploadAttachmentRequestValidator.cs
@@ -10,7 +10,7 @@ namespace ColdChainX.Application.Validators
     public class UploadAttachmentRequestValidator : AbstractValidator<UploadAttachmentRequest>
     {
         private static readonly string[] AllowedExtensions = { ".jpg", ".jpeg", ".png", ".pdf" };
-        private const long MaxFileSizeBytes = 50 * 1024 * 1024; // 50 MB
+        private const long MaxFileSizeBytes = 10 * 1024 * 1024; // 10 MB
 
         public UploadAttachmentRequestValidator()
         {
@@ -19,7 +19,7 @@ namespace ColdChainX.Application.Validators
 
             RuleFor(x => x.File)
                 .Must(file => file.Length > 0).WithMessage("File cannot be empty.")
-                .Must(file => file.Length <= MaxFileSizeBytes).WithMessage("File size must not exceed 50 MB.")
+                .Must(file => file.Length <= MaxFileSizeBytes).WithMessage("File size must not exceed 10 MB.")
                 .Must(file =>
                 {
                     var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
