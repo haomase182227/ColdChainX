efec7edb95a85b48c0a75535c51c1f01d4fb1d57
ThangDVSE183732 <thangdvse183732@fpt.edu.vn>
2026-07-14 21:50:51 +0700

    chore: Cập nhật thông tin kết nối MQTT và Redis sang máy chủ GCP, kích hoạt lại IoT Background Workers

--- ColdChainX.API/Program.cs
+++ ColdChainX.API/Program.cs
@@ -35,22 +35,22 @@ builder.Services.AddSingleton(Channel.CreateUnbounded<TelemetryData>(new Unbound
 }));
 builder.Services.AddSingleton<RedisService>();
 
-// Tạm thời tắt TelemetryMqttWorker vì lỗi trên Azure Docker
-// if (configuration.GetValue("HostedWorkers:TelemetryMqtt", true))
-// {
-//     builder.Services.AddHostedService<TelemetryMqttWorker>();
-// }
+// Đã bật lại TelemetryMqttWorker
+if (configuration.GetValue("HostedWorkers:TelemetryMqtt", true))
+{
+    builder.Services.AddHostedService<TelemetryMqttWorker>();
+}
 
 if (configuration.GetValue("HostedWorkers:TelemetryProcessor", true))
 {
     builder.Services.AddHostedService<TelemetryProcessorWorker>();
 }
 
-// Tạm thời tắt IotWatchdogWorker vì lỗi trên Azure Docker
-// if (configuration.GetValue("HostedWorkers:IotWatchdog", true))
-// {
-//     builder.Services.AddHostedService<IotWatchdogWorker>();
-// }
+// Đã bật lại IotWatchdogWorker
+if (configuration.GetValue("HostedWorkers:IotWatchdog", true))
+{
+    builder.Services.AddHostedService<IotWatchdogWorker>();
+}
 
 if (configuration.GetValue("HostedWorkers:InventoryAging", true))
 {
