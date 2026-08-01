a16e3f0f98be36583e0ec7550c68769b7616b869
ThangDVSE183732 <thangdvse183732@fpt.edu.vn>
2026-08-01 11:04:23 +0700

    Update ClaimsController.cs, ClaimService.cs and IncidentAndClaimServiceTests.cs

--- ColdChainX.Application/DTOs/Claim/CreateClaimRequest.cs
+++ ColdChainX.Application/DTOs/Claim/CreateClaimRequest.cs
@@ -1,13 +1,23 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 
 namespace ColdChainX.Application.DTOs.Claim
 {
+    public enum ClaimCategory
+    {
+        DAMAGE,
+        QUALITY_VIOLATION,
+        LOSS,
+        DELAY,
+        WRONG_ITEM
+    }
+
     public class CreateClaimRequest
     {
         public Guid? OrderId { get; set; }
-        public string ClaimType { get; set; } = null!;
+        public ClaimCategory ClaimType { get; set; }
         public string Description { get; set; } = null!;
-        public List<string> EvidenceImages { get; set; } = new();
+        public List<IFormFile> EvidenceImages { get; set; } = new();
     }
 }
